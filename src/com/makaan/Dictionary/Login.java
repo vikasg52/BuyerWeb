0491d64fcad2b2afd2b1c09c8c03baa3317c9652
vikas <vikas@PT-MAC-42.local>
2016-05-06 16:37:34 +0530

    first commit

---
+++ src/com/makaan/Dictionary/Login.java
@@ -0,0 +1,22 @@
+package com.makaan.Dictionary;
+
+public class Login {
+	
+	public static String MakaanLogo =".//div[@class='makaan-logo']";
+	
+	public static String Login = ".//div[@class='login-wrap clearfix']//div[@class='signin']";
+	
+	public static String Social_Login_validate = ".//div[@id='sociable-connect']/div[@class='mian-field-box']";
+	
+	public static String faebookLogin = ".//span[@class='icon icon-facebook']";
+	
+	public static String GoogleLogin = "//span[@class='icon icon-google-plus']";
+	
+	public static String faebookLoginForm = ".//div[@class='login_form_container']";
+	
+	public static String GoogleLoginForm= ".//div[@class='logo logo-w']";
+	
+	
+	
+
+}
